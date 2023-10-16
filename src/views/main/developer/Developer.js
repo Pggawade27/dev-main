d00516bb2f736be43a37508d5c82a8bd54962ca7
prasad <prasadgawade249@gmail.com>
2023-10-16 16:38:14 +0530

    first commit

---
+++ src/views/main/developer/Developer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Section2 } from './componets/section2/Section2'
+import { Section3 } from './componets/section3/Section3'
+
+export const Developer = () => {
+  return (
+    <>
+        <Section2/>
+        <Section3/>
+    </>
+  )
+}
